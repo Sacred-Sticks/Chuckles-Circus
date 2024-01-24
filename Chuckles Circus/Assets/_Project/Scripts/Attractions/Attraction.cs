eb619d2635a9561f343a13914aacc4234ea1ce45
Lucas Ackman <AckmanLD@gmail.com>
2024-01-24 14:45:39 -0600

    Added data to Attraction Scriptable Object

--- Chuckles Circus/Assets/_Project/Scripts/Attractions/Attraction.cs
+++ Chuckles Circus/Assets/_Project/Scripts/Attractions/Attraction.cs
@@ -3,5 +3,8 @@
 [CreateAssetMenu(menuName = "Chuckles Circus/Attracion", fileName = "Attraction", order = 0)]
 public class Attraction : ScriptableObject
 {
-    
+    [SerializeField] private GameObject attractionPrefab;
+    [SerializeField] private Shader attractionShader;
+    [SerializeField] private Shader hologramShader;
+    [SerializeField] private float costToBuild;
 }
