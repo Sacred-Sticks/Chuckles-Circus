cc0be8368a7276f0a113fe0045d468eb94b8e0ae
Lucas Ackman <AckmanLD@gmail.com>
2024-01-25 19:41:14 -0600

    Added Terrain and Borders to Camera Movement

--- Chuckles Circus/Assets/_Project/Scripts/Camera/Locomotion.cs
+++ Chuckles Circus/Assets/_Project/Scripts/Camera/Locomotion.cs
@@ -1,10 +1,14 @@
+using System;
+using System.Security.Authentication.ExtendedProtection;
 using Kickstarter.Inputs;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
 public class Locomotion : MonoBehaviour, IInputReceiver, ILocomotion
 {
     [SerializeField] private Vector2Input movementInput;
     [field: SerializeField] public float Speed { get; set; }
+    [SerializeField] private Bounds bounds;
     
     private Rigidbody body;
     private Vector3 rawInput;
@@ -16,9 +20,14 @@ public class Locomotion : MonoBehaviour, IInputReceiver, ILocomotion
         body.useGravity = false;
     }
 
+    private void Update()
+    {
+        KeepWithinBorder();
+    }
+
     private void FixedUpdate()
     {
-        body.velocity = rawInput * Speed;
+        MoveCamera();
     }
     #endregion
     
@@ -38,6 +47,25 @@ public class Locomotion : MonoBehaviour, IInputReceiver, ILocomotion
         rawInput = new Vector3(input.x, 0, input.y);
     }
     #endregion
+
+    private void MoveCamera()
+    {
+        body.velocity = rawInput * Speed;
+    }
+
+    private void KeepWithinBorder()
+    {
+        var rootPosition = transform.root.position;
+        if (rootPosition.x < bounds.center.x - bounds.extents.x)
+            rootPosition.x = bounds.center.x - bounds.extents.x;
+        if (rootPosition.x > bounds.center.x + bounds.extents.x)
+            rootPosition.x = bounds.center.x + bounds.extents.x;
+        if (rootPosition.z < bounds.center.z - bounds.extents.z)
+            rootPosition.z = bounds.center.z - bounds.extents.z;
+        if (rootPosition.z > bounds.center.z + bounds.extents.z)
+            rootPosition.z = bounds.center.z + bounds.extents.z;
+        transform.root.position = rootPosition;
+    }
 }
 
 public interface ILocomotion
